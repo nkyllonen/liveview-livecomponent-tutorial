a26e8b8e74dd8c906b82c81ef0500267bb7ba48c
nkyllonen <nikki-kyllonen@comcast.net>
2021-03-01 22:37:09 -0800

    Extend counter to sync across multiple users

--- lib/live_view_counter_web/live/controller.ex
+++ lib/live_view_counter_web/live/controller.ex
@@ -2,12 +2,17 @@ defmodule LiveViewCounterWeb.Counter do
   # use the Phoenix.LiveView behavior
   use Phoenix.LiveView
 
+  # define a module attribute (global constant)
+  @topic "live"
+
   @doc """
     mount/3: mounts the module and returns a tuple
       - :ok
       - assign a key :val to the socket (initialized to 0)
   """
   def mount(_params, _session, socket) do
+    # subscribe to the channel topic
+    LiveViewCounterWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
@@ -18,7 +23,11 @@ defmodule LiveViewCounterWeb.Counter do
           &(&1 + 1) is the same as fn val -> val + 1 end
   """
   def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+    new_state = update(socket, :val, &(&1 + 1))  # instance of Phoenix.LiveView.Socket
+    # send a message from current process self() on the @topic
+    # the key is "inc" the value is the new_state.assigns Map
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   @doc """
@@ -28,7 +37,20 @@ defmodule LiveViewCounterWeb.Counter do
           &(&1 - 1) is the same as fn val -> val - 1 end
   """
   def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+    new_state = update(socket, :val, &(&1 - 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  @doc """
+    handle_info/2: handles Elixir process messages where msg is the received message
+                    and socket is the Phoenix.Socket
+      - :noreply: "don't send this message to the socket again"
+                  which would cause a recursive loop of updates)
+      - add key value pair (val, msg.payload.val) to socket assigns
+  """
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   @doc """
